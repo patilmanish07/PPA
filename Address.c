0e947a4d6898f14d321d90b418d0fea3691bcfca
Manish patil <99861707+patilmanish07@users.noreply.github.com>
2022-06-10 01:41:24 +0530

    Add files via upload

---
+++ Address.c
@@ -0,0 +1,20 @@
+ 
+ #include<stdio.h>
+ 
+
+  int main()
+ {
+   int no = 11;
+   
+   char ch = 'A';
+   
+   printf("%d",no);
+ 
+   printf("%c",ch);
+   
+   printf("%u\n",&no);
+   
+   printf("%u\n",&ch);
+ 
+    return 0;
+	}
